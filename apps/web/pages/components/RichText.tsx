32d97db6d072dda602fe995be6d008739cac3ddf
pegasis <me@pegasis.site>
2022-05-25 08:54:37 -0400

    fix rich text key

--- apps/web/pages/components/RichText.tsx
+++ apps/web/pages/components/RichText.tsx
@@ -21,43 +21,43 @@ function RichTextNode(props: { data: RichTextData }) {
     {props.data.map((node, i) => {
       if ('text' in node) {
         if (node.bold){
-          return <strong>
+          return <strong key={i}>
             {node.text}
           </strong>
         }
         if (node.italic){
-          return <em>
+          return <em key={i}>
             {node.text}
           </em>
         }
         if (node.underline){
-          return <u>
+          return <u key={i}>
             {node.text}
           </u>
         }
-        return <span>
+        return <span key={i}>
           {node.text}
         </span>
       }
 
       if (node.type === 'ol') {
-        return <List type={'ordered'} mt={'sm'}>
+        return <List type={'ordered'} mt={'sm'} key={i}>
           {node.children.map((child: any, i: number) => <List.Item key={i}>
             <RichTextNode data={child.children}/>
           </List.Item>)}
         </List>
       } else if (node.type === 'ul') {
-        return <List type={'unordered'} mt={'sm'}>
+        return <List type={'unordered'} mt={'sm'} key={i}>
           {node.children.map((child: any, i: number) => <List.Item key={i}>
             <RichTextNode data={child.children}/>
           </List.Item>)}
         </List>
       } else if (node.type === 'link') {
-        return <Anchor target={'_blank'} rel={'noreferrer'} href={node.url}>
+        return <Anchor target={'_blank'} rel={'noreferrer'} href={node.url} key={i}>
           <RichTextNode data={node.children}/>
         </Anchor>
       } else {
-        return <Text mt={'sm'}>
+        return <Text mt={'sm'} key={i}>
           <RichTextNode data={node.children}/>
         </Text>
       }
