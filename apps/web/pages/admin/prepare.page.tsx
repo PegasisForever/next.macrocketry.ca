c94d6ff7ba1171783f97f1bf56ee8e857a37aa72
pegasis <me@pegasis.site>
2022-06-19 18:34:00 -0400

    use react-fetch-hook

--- apps/web/pages/admin/prepare.page.tsx
+++ apps/web/pages/admin/prepare.page.tsx
@@ -2,44 +2,45 @@ import {Button, Loader, Stack, Text, Title, useMantineTheme} from '@mantine/core
 import {useEffect} from 'react'
 import {useRouter} from 'next/router'
 import {useModals} from '@mantine/modals'
+import useFetch from 'react-fetch-hook'
 
 export default function PrepareAdmin() {
+  const {error, data} = useFetch('/admin', {
+    formatter: (response) => response.text(),
+  })
   const router = useRouter()
   const theme = useMantineTheme()
   const modals = useModals()
 
   useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch('/admin')
-        if (res.status === 200) {
-          router.replace('/admin')
-        } else {
-          throw res
-        }
-      } catch (e) {
-        console.error(e)
-        modals.openModal({
-          centered: true,
-          overlayBlur: 10,
-          overlayColor: theme.fn.rgba(theme.black, 0.4),
-          withCloseButton: false,
-          padding: 'xl',
-          children: <Stack align={'center'}>
-            <Title order={2} sx={{
-              fontWeight: 500,
-              textAlign: 'center',
-            }}>
-              Failed to prepare admin panel
-            </Title>
-            <Button size={'md'} onClick={() => router.reload()}>
-              Retry
-            </Button>
-          </Stack>,
-        })
-      }
-    })()
-  }, [])
+    if (data) {
+      router.replace('/admin')
+    }
+  }, [router, data])
+
+  useEffect(() => {
+    if (error) {
+      console.error(error)
+      modals.openModal({
+        centered: true,
+        overlayBlur: 10,
+        overlayColor: theme.fn.rgba(theme.black, 0.4),
+        withCloseButton: false,
+        padding: 'xl',
+        children: <Stack align={'center'}>
+          <Title order={2} sx={{
+            fontWeight: 500,
+            textAlign: 'center',
+          }}>
+            Failed to prepare admin panel
+          </Title>
+          <Button size={'md'} onClick={() => router.reload()}>
+            Retry
+          </Button>
+        </Stack>,
+      })
+    }
+  }, [error])
 
   return <Stack align={'center'} justify={'center'} sx={{
     width: '100vw',
