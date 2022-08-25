385ddfd1535786fc194be476fef197f48f37bb34
pegasis <me@pegasis.site>
2022-08-25 10:11:08 -0400

    center images

--- apps/web/pages/avionics/void-lake-5/index.page.tsx
+++ apps/web/pages/avionics/void-lake-5/index.page.tsx
@@ -7,9 +7,9 @@ import pcbSchema from './images/pcb_schema.png'
 import pcbLayout from './images/pcb_layout.png'
 import pcb2d from './images/pcb_2d.png'
 import Image from 'next/image'
-import {StaticImageData} from 'next/dist/client/image'
 import {Sx} from '@mantine/styles/lib/theme/types/DefaultProps'
-import {PropsWithChildren, useRef} from 'react'
+import {forwardRef, PropsWithChildren, ReactNode, useRef} from 'react'
+import {useMeasure, useWindowSize} from 'react-use'
 
 export default function VoidLake5Page() {
   const theme = useMantineTheme()
@@ -17,6 +17,7 @@ export default function VoidLake5Page() {
 
   return <RightPanelContainer hrefIndex={3} sx={{
     overflowY: 'hidden',
+    backgroundColor: theme.black,
   }}>
     <Box ref={scrollContainerRef} sx={{
       position: 'relative',
@@ -26,8 +27,11 @@ export default function VoidLake5Page() {
       height: '100vh',
       overflowY: 'auto',
     }}>
-      <ParallaxImage src={pcbSchema}>
-        <Stack spacing={8} align={'center'} justify={'center'} sx={{
+      <ParallaxContainer
+        sx={{
+          height: '100vh',
+        }}
+        noneParallax={<Stack spacing={8} align={'center'} justify={'center'} sx={{
           width: '100%',
           height: '100%',
           backgroundColor: theme.fn.rgba(theme.black, 0.5),
@@ -58,46 +62,94 @@ export default function VoidLake5Page() {
           <Text size={'lg'}>
             Striving Perfection
           </Text>
-        </Stack>
-      </ParallaxImage>
-      <ParallaxImage src={pcbLayout} sx={{
-        background: theme.black,
-      }}/>
-      <ParallaxImage src={pcb2d} sx={{
-        background: theme.black,
+        </Stack>}>
+        <Image
+          priority
+          src={pcbSchema}
+          placeholder={'blur'}
+          layout={'fill'}
+        />
+      </ParallaxContainer>
+      <PCBLayoutSection/>
+      <PCB2DSection/>
+      <Box sx={{
+        height: '100vh',
       }}/>
     </Box>
   </RightPanelContainer>
 }
 
-function ParallaxImage(props: PropsWithChildren<{ src: StaticImageData, sx?: Sx }>) {
-  return <Box sx={[{
-    position: 'relative',
-    height: '100vh',
-    width: '100%',
-    backfaceVisibility: 'hidden',
-    overflow: 'hidden',
-    clipPath: 'inset(0 0 0 0)',
-  }, props.sx]}>
-    <Box sx={{
-      position: 'fixed',
-      left: 0,
-      top: 0,
-      width: '100%',
-      height: '100vh',
-      pointerEvents: 'none',
+function PCBLayoutSection() {
+  const [ref, {width: imageWidth}] = useMeasure<HTMLDivElement>()
+  const {height: windowHeight} = useWindowSize()
+
+  const imageHeight = pcbLayout.height / (pcbLayout.width / imageWidth)
+  const paddingTop = (windowHeight - imageHeight) / 2
+
+  return <ParallaxContainer
+    ref={ref}
+    sx={{
+      height: imageHeight + paddingTop,
+    }}
+    innerSx={{
+      paddingTop,
     }}>
-      <Image
-        src={props.src}
-        placeholder={'blur'}
-        layout={'responsive'}
-        priority
-        loading={'eager'}
-      />
-    </Box>
-    {props.children}
-  </Box>
+    <Image
+      priority
+      src={pcbLayout}
+      placeholder={'blur'}
+      layout={'responsive'}
+    />
+  </ParallaxContainer>
 }
+
+function PCB2DSection() {
+  const [ref, {width: imageWidth}] = useMeasure<HTMLDivElement>()
+  const {height: windowHeight} = useWindowSize()
+
+  const imageHeight = pcbLayout.height / (pcbLayout.width / imageWidth)
+  const paddingTop = (windowHeight - imageHeight) / 2
+
+  return <ParallaxContainer
+    ref={ref}
+    sx={{
+      height: imageHeight,
+    }}
+    innerSx={{
+      height: imageHeight + paddingTop,
+      paddingTop,
+    }}>
+    <Image
+      priority
+      src={pcb2d}
+      placeholder={'blur'}
+      layout={'responsive'}
+    />
+  </ParallaxContainer>
+}
+
+const ParallaxContainer = forwardRef<HTMLDivElement, PropsWithChildren<{ sx?: Sx, innerSx?: Sx, noneParallax?: ReactNode }>>(function ParallaxContainer(props, ref) {
+    return <Box ref={ref} sx={[{
+      position: 'relative',
+      width: '100%',
+      backfaceVisibility: 'hidden',
+      overflow: 'hidden',
+      clipPath: 'inset(0 0 0 0)',
+    }, props.sx]}>
+      <Box sx={[{
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        pointerEvents: 'none',
+      }, props.innerSx]}>
+        {props.children}
+      </Box>
+      {props.noneParallax}
+    </Box>
+  },
+)
 
 export const getStaticProps: GetStaticProps<PropsWithSideBar<{}>> = async () => {
   return {
